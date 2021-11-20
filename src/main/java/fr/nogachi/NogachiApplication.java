718d172566c1ca2d831287d08e345bded1c06b69
Elvis Messiaen <elvism72@gmail.com>
2021-11-20 17:10:28 +0100

    first commit

---
+++ src/main/java/fr/nogachi/NogachiApplication.java
@@ -0,0 +1,13 @@
+package fr.nogachi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NogachiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NogachiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/nogachi/NogachiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nogachi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NogachiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
