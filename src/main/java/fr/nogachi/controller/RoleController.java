6301aaf34c4872ff9d62c07d465cf6042334a426
willy <willy.quasevi@gmail.com>
2021-11-28 16:13:43 +0100

    class role done

--- src/main/java/fr/nogachi/controller/RoleController.java
+++ src/main/java/fr/nogachi/controller/RoleController.java
@@ -16,35 +16,35 @@ public class RoleController {
     public RoleController(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
-
+    // affiche la liste des roles check
     @GetMapping(path = "/role")
-    public List<Role> roleList(){
+    public List<Role> roleListe(){
         return  roleRepository.findAll();
     }
 
-    @GetMapping(path = "/role{id}")
-    public Role afficherUnUtilisateur(@PathVariable Long id) {
-
+    // affiche un utilisateur par son id check
+    @GetMapping(path = "/role/{id}")
+    public Role getRole(@PathVariable Long id) {
         return roleRepository.findById(id).get();
     }
 
 // sauvergarde d'un utilisateur par son id
-    /*
-
-     */
-    @PutMapping(path = "/role{id}")
-    public ResponseEntity<Role> updateTodo(@PathVariable Long id, @RequestBody Role role) {
-        roleRepository.save(role);
-        return new ResponseEntity<>(roleRepository.getById((id)), HttpStatus.OK);
+    @PostMapping(path = "/role")
+    public Role save(@RequestBody Role role) {
+        return roleRepository.save(role);
     }
 
-    @DeleteMapping(path = "/role{id}")
-    public void deleteRole(@PathVariable Role role) {
-        roleRepository.delete(role);
+    // mise a jour role check
+    @PutMapping(path = "/role/{id}")
+    public Role update(@PathVariable Long id, @RequestBody Role role) {
+        role.setId(id);
+        return roleRepository.save(role);
     }
 
-/*    @RequestMapping(value = "role{id}", method = RequestMethod.DELETE)
-    public void deletePlace(@PathVariable(value = "id") Long id) {
-        this.roleRepository.deleteRole(id);
-    }*/
+    // supprime un utilisateur check
+    @DeleteMapping(path = "/role/{id}")
+    public void delete(@PathVariable Long id) { roleRepository.deleteById(id);
+    }
+
+
 }
