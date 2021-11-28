d18c6c1503d19f53725acbd7783d52aadf1bdb39
Elvis Messiaen <elvism72@gmail.com>
2021-11-28 17:26:55 +0100

    Commentaire : ok

--- src/main/java/fr/nogachi/repository/UtilisateurRepository.java
+++ src/main/java/fr/nogachi/repository/UtilisateurRepository.java
@@ -2,6 +2,9 @@ package fr.nogachi.repository;
 
 import fr.nogachi.entity.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ * @author Messiaen ELVIS
+ */
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
 
