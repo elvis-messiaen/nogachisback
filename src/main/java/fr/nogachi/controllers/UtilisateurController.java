ed41bb9dd79042c1f13e6df3a1ec45e9a50437cb
Elvis <elvism72@gmail.com>
2021-12-10 14:28:23 +0100

    feat: service utilisateur

--- src/main/java/fr/nogachi/controllers/UtilisateurController.java
+++ src/main/java/fr/nogachi/controllers/UtilisateurController.java
@@ -1,9 +1,11 @@
 package fr.nogachi.controllers;
 
 import fr.nogachi.entities.Utilisateur;
-import fr.nogachi.repositories.UtilisateurRepository;
+import fr.nogachi.services.UtilisateurService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Messiaen elvis
@@ -12,66 +14,70 @@ import java.util.List;
 @RestController
 public class UtilisateurController {
 
-    private UtilisateurRepository utilisateurRepository;
+
+    public UtilisateurController(UtilisateurService utilisateurService) {
+        this.utilisateurService = utilisateurService;
+    }
+
+    UtilisateurService utilisateurService;
 
     /**
      *   instancie à la demande un utilisateur repository
      *   POSTMAN: OK
      *   TEST : (OK/FAIL/WIP)
      */
-    public UtilisateurController(UtilisateurRepository utilisateurRepository) {
-        this.utilisateurRepository = utilisateurRepository;
-    }
 
     /**
-     *   affiche la list des utilisateurs
-     *   POSTMAN: OK
-     *   TEST : (OK/FAIL/WIP)
+     * affiche la list des utilisateurs
+     * POSTMAN: OK
+     * TEST : (OK/FAIL/WIP)
      */
     @GetMapping(path = "/utilisateur")
     public List<Utilisateur> ListUtilisateur() {
-        return utilisateurRepository.findAll();
+        return utilisateurService.lister();
+
     }
 
     /**
-     *   recherche un utilisateur par id
-     *   POSTMAN: OK
-     *   TEST : (OK/FAIL/WIP)
+     * recherche un utilisateur par id
+     * POSTMAN: OK
+     * TEST : (OK/FAIL/WIP)
+     *
+     * @return
      */
     @GetMapping(path = "/utilisateur/{id}")
-    public Utilisateur afficherUnUtilisateur(@PathVariable Long id) {
-
-        return utilisateurRepository.findById(id).get();
+    public Optional<Utilisateur> afficherUnUtilisateur(@PathVariable Long id) {
+        return utilisateurService.trouver(id);
     }
 
     /**
-     *   ajouter un utilisateur
-     *   POSTMAN: OK
-     *   TEST : (OK/FAIL/WIP)
+     * ajouter un utilisateur
+     * POSTMAN: OK
+     * TEST : (OK/FAIL/WIP)
      */
     @PostMapping(path = "/utilisateur")
-    public Utilisateur ajouter (@RequestBody Utilisateur utilisateur) {
-        return  utilisateurRepository.save(utilisateur);
+    public Utilisateur ajouter(@RequestBody Utilisateur utilisateur) {
+        return utilisateurService.enregistrer(utilisateur);
     }
 
     /**
-     *   mise a jour de l'utilisateur
-     *   POSTMAN: OK
-     *   TEST : (OK/FAIL/WIP)
+     * mise a jour de l'utilisateur
+     * POSTMAN: OK
+     * TEST : (OK/FAIL/WIP)
      */
     @PutMapping(path = "/utilisateur/{id}")
-    public Utilisateur miseAJour (@PathVariable Long id, @RequestBody Utilisateur utilisateur){
+    public Utilisateur miseAJour(@PathVariable Long id, @RequestBody Utilisateur utilisateur) {
         utilisateur.setId(id);
-        return utilisateurRepository.save(utilisateur);
+        return utilisateurService.enregistrer(utilisateur);
     }
 
     /**
-     *   suppprimer un utilisateur
-     *   POSTMAN: OK
-     *   TEST : (OK/FAIL/WIP)
+     * suppprimer un utilisateur
+     * POSTMAN: OK
+     * TEST : (OK/FAIL/WIP)
      */
     @DeleteMapping(path = "/utilisateur/{id}")
-    public void supprimerUtilisateur (@PathVariable Long id){
-        utilisateurRepository.deleteById(id);
+    public void supprimerUtilisateur(@PathVariable Long id) {
+        utilisateurService.supprimer(id);
     }
 }
