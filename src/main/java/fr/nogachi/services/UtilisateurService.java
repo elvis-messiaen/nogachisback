29e13749126e213b701b15aa8e2fe0f2d79cca84
Elvis <elvism72@gmail.com>
2021-12-12 18:33:04 +0100

    git commit -m "fix : service utilisateur refractor"

--- src/main/java/fr/nogachi/services/UtilisateurService.java
+++ src/main/java/fr/nogachi/services/UtilisateurService.java
@@ -2,34 +2,66 @@ package fr.nogachi.services;
 
 import fr.nogachi.entities.Utilisateur;
 import fr.nogachi.repositories.UtilisateurRepository;
-import fr.nogachi.services.impl.UtilisateurServiceImp;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
-public class UtilisateurService implements UtilisateurServiceImp {
-    @Autowired
+@Service
+public class UtilisateurService implements fr.nogachi.services.impl.UtilisateurService {
+    /**
+     * instance de la méthode utilisateurRepository
+     */
     private UtilisateurRepository utilisateurRepository;
 
-   @Transactional
+    /**
+     * constructeur du repository
+     * @param utilisateurRepository
+     */
+    public UtilisateurService(UtilisateurRepository utilisateurRepository) {
+        this.utilisateurRepository = utilisateurRepository;
+    }
+    /**
+     * Transactional => verifie qu'il n'y a pas de problème lors de la transaction
+     * si un probleme annule toute modification
+     * @param utilisateur
+     * @return sauvegarde de l'utilisateur
+     */
+    @Transactional
     public Utilisateur enregistrer(Utilisateur utilisateur) {
+
        return utilisateurRepository.save(utilisateur);
     }
 
+    /**
+     * supprime l'utilisateur par son id
+     * @param id
+     */
     @Transactional
     public void supprimer(Long id) {
-        utilisateurRepository.deleteById(id);
+
+       utilisateurRepository.deleteById(id);
     }
 
+    /**
+     * affiche une liste d'utilisateur
+     * @return la listes des utilisateurs
+     */
     @Transactional
     public List<Utilisateur> lister() {
-        return utilisateurRepository.findAll();
+
+       return utilisateurRepository.findAll();
     }
 
+    /**
+     *  récuperer un utlisateur par son id
+     * @param id
+     * @return
+     */
     @Transactional
     public Optional<Utilisateur> trouver(Long id) {
-        return utilisateurRepository.findById(id);
+
+       return utilisateurRepository.findById(id);
     }
 }
--- src/main/java/fr/nogachi/services/impl/UtilisateurService.java
+++ src/main/java/fr/nogachi/services/impl/UtilisateurService.java
@@ -5,7 +5,7 @@ import fr.nogachi.entities.Utilisateur;
 import java.util.List;
 import java.util.Optional;
 
-public interface UtilisateurServiceImp {
+public interface UtilisateurService {
     Utilisateur enregistrer(Utilisateur utilisateur);
 
     void supprimer(Long id);
