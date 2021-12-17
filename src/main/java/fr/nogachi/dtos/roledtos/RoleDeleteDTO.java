61408d829fec1f00be81796993af7a12b4aa5ff8
wil <willy.quasevi@gmail.com>
2021-12-17 16:04:51 +0100

    ADD: DTO ROLE FIX: correction WIP: utilisateurService

---
+++ src/main/java/fr/nogachi/dtos/roledtos/RoleDeleteDTO.java
@@ -0,0 +1,15 @@
+package fr.nogachi.dtos.roledtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RoleDeleteDTO {
+
+    private Long id;
+
+}
---
+++ src/main/java/fr/nogachi/dtos/roledtos/RoleUpdateDTO.java
@@ -0,0 +1,15 @@
+package fr.nogachi.dtos.roledtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class RoleUpdateDTO {
+
+    private Long id;
+    private String nom;
+}
