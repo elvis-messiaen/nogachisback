6738ef0e58d600642e0ca582d0da476e817fb7c1
Elvis <elvism72@gmail.com>
2022-02-15 20:41:22 +0100

    Revert "WIP"
    This reverts commit 68e115f14d8508516174b79701debee97a730955.

--- src/main/java/fr/nogachi/controllers/AuthController.java
+++ src/main/java/fr/nogachi/controllers/AuthController.java
@@ -50,7 +50,7 @@ public class AuthController {
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
